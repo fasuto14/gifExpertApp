11fbbcaa38ac9c8d7cb09146e83cafb0349b1a17
Fausto Azzaretti <faustoazzaretti951@gmail.com>
2022-06-04 10:36:38 -0300

    Add useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 
 export const GifGrid = ({category}) => {
-
-
+    const [count,setCount] = useState(0);
+    useEffect(()=>{
+        getGifs();
+    },[])
     const getGifs = async() => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Pokemon&api_key=fKDA0Yv7toq28ACZ4p3HXRTvhl8W0DxT&limit=10';
         const resp = await fetch(url);
@@ -18,8 +20,11 @@ export const GifGrid = ({category}) => {
         console.log(gifs);
     }
 
-    getGifs();
     return (
-    <h3>{category}</h3>
+    <>
+        <h3>{category}</h3>
+        <h2>{count}</h2>
+        <button onClick={()=> {setCount(count+1)}}>Count</button>
+    </>
   )
 }
