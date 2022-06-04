72229a8da92dd00d8fd9596d4240ae565a4ce455
Fausto Azzaretti <faustoazzaretti951@gmail.com>
2022-06-04 09:31:38 -0300

    Setup Configuration and add GifExpertApp component

---
+++ src/GifExpertApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GifExpertApp = () => {
+    return(
+        <>
+            <h1>GifExpertApp</h1>
+            <h2>React Practice</h2>
+            <hr/>
+        </>
+    )
+}
+
+export default GifExpertApp;
